b6ebd67cc7ded711be6b24e5f2f9ae1498b5cd9a
Harsh Jain <hvj.techture@gmail.com>
2023-11-19 06:32:29 +0530

    - inital setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Scroll, ScrollControls } from "@react-three/drei";
 
-function App() {
+const App = () => {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={4} damping={0.1}>
+          <Experience />
+          <Scroll html>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,12 +2,9 @@ import { OrbitControls } from "@react-three/drei";
 
 export const Experience = () => {
   return (
-    <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
+    <mesh>
+      <boxGeometry />
+      <meshNormalMaterial />
+    </mesh>
   );
 };
