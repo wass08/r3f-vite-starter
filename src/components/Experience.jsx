a8f7866830eeb65ebf3f67827955d90b293b40af
rumaisachowdhury <rumaisa.chowdhury@mail.utoronto.ca>
2024-03-30 15:18:48 -0400

    Initial Commit - Model with demo audio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { Avatar } from "./Avatar";
 
 export const Experience = () => {
+  const texture = useTexture("textures/black.jpg");
+  const viewport = useThree((state) => state.viewport);
+
   return (
     <>
       <OrbitControls />
+      <Avatar position={[0, -3, 5]} scale={2} />
+      <Environment preset="sunset" />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <planeGeometry args={[viewport.width, viewport.height]} />
+        <meshBasicMaterial map={texture} />
       </mesh>
     </>
   );
