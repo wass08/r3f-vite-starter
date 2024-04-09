26dfb2c753b3ca31ca8d500c7128f5d6585cfb4b
rumaisachowdhury <rumaisa.chowdhury@mail.utoronto.ca>
2024-04-09 14:43:10 -0400

    added theatre js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-import {Snowing} from "./components/Snowing.jsx";
-import {OrbitControls, Stars} from "@react-three/drei";
-import {Snowfall} from "./components/Snowflake.jsx";
+import { Snowing } from "./components/Snowing.jsx";
+import { OrbitControls, Stars, Environment } from "@react-three/drei";
+import { Snowfall } from "./components/Snowflake.jsx";
+import { getProject } from "@theatre/core";
+import studio from "@theatre/studio";
+import extension from "@theatre/r3f/dist/extension";
+import { editable as e, SheetProvider } from "@theatre/r3f";
+import { Avatar } from "./components/Avatar";
 
 const sizes = {
   width: window.innerWidth,
-  height: window.innerHeight
+  height: window.innerHeight,
 };
+
+const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
+
+studio.initialize();
+studio.extend(extension);
 
 function App() {
   return (
-      <Canvas className="canvas">
-          <color attach="background" args={["#000"]} />
-          <perspectiveCamera
-            fov={75}
-            aspect={sizes.width / sizes.height}
-            position={[0, 0, 3]}
-            near={0.1}
-            far={100}
-          >
-            <Snowfall />
-              <Experience />
-          </perspectiveCamera>
-          <ambientLight />
-          <OrbitControls />
+    <Canvas className="canvas">
+      <SheetProvider sheet={demoSheet}>
+        <color attach="background" args={["#000"]} />
+        <perspectiveCamera
+          fov={75}
+          aspect={sizes.width / sizes.height}
+          position={[0, 0, 3]}
+          near={0.1}
+          far={100}
+        >
+          <Avatar position={[0, -3, 5]} scale={2} />
+          <Stars />
+          {/* <e.pointLight theatreKey="Light" position={[10, 10, 10]} /> */}
+          <Environment preset="sunset" />
+          {/* <Snowfall /> */}
+          {/* <Experience /> */}
+        </perspectiveCamera>
+        <ambientLight />
+        <OrbitControls />
+      </SheetProvider>
     </Canvas>
-    // <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-    //   <color attach="background" args={["#ececec"]} />
-    //   <Experience />
-    // </Canvas>
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,7 @@
 import { Environment, OrbitControls, useTexture } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { Avatar } from "./Avatar";
-import {Snowing} from "./Snowing.jsx";
+import { Snowing } from "./Snowing.jsx";
 
 export const Experience = () => {
   const texture = useTexture("textures/black.jpg");
@@ -12,10 +12,10 @@ export const Experience = () => {
       <OrbitControls />
       <Avatar position={[0, -3, 5]} scale={2} />
       <Environment preset="sunset" />
-      <mesh>
+      {/* <mesh>
         <planeGeometry args={[viewport.width, viewport.height]} />
         <meshBasicMaterial map={texture} />
-      </mesh>
+      </mesh> */}
     </>
   );
 };
