44ef016c0d20211f3d012510016332fa3a2d2b05
Axel Gumiit <149522466+AxelGumiit@users.noreply.github.com>
2024-08-20 08:08:35 +0100

    import a character with animation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,38 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import{useControls} from "leva"
 
 export const Experience = () => {
+
+  const {animations} =  useControls({
+    animations:{
+
+    
+    value:"Typing",
+    options: ["Typing", "Falling", "Standing"]
+    },
+  })
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <OrbitControls/>
+      <Sky/>
+      <Environment preset ="sunset"></Environment>
+      <group position-y={-1}>
+      <Avatar animations = {animations}/>
+
+      {animations === "Typing" &&(
+        <mesh scale = {[0.8,0.5,0.8]} position-y = {0.25} >
+        <boxGeometry/>
+        <meshStandardMaterial color = "white"/>
       </mesh>
+
+      )}
+      
+      <mesh scale={5} rotation-x = {Math.PI * 0.5}>
+        <planeGeometry/>
+        <meshStandardMaterial color ="white" />
+      </mesh>
+      </group>
     </>
   );
 };
