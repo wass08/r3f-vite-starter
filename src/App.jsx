2917982c639134b73134c707f1f3ef0929bab1df
Yusuf Ahmed <79456680+MoeJango@users.noreply.github.com>
2024-10-18 19:27:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,15 @@ import React, { useState, useRef, useEffect } from "react";
 import { Sphere, Box } from "@react-three/drei"; // Import Sphere from drei
 import Model from "./assets/track/FullRaceTrackRaw";
 import { Race } from "./assets/track/Racw";
+import HUD from "./components/HUD";
 
 
 // Main App
 function App() {
 
   return (
-    <Canvas shadows camera={{ position: [3, 3, 10], fov: 50 }}>
-      <color attach="background" args={["#ececec"]} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
-
-      {/* Enable OrbitControls for camera movement */}
-      <OrbitControls enableZoom={true} enableRotate={true} />
-
-      {/* Add a cube */}
-      <Box position={[0, 1, 0]} castShadow receiveShadow>
-        <meshStandardMaterial attach="material" color="orange" />
-      </Box>
-        <Model />
-        <Race />
-    </Canvas>
+    <HUD/>
+    
   );
 }
 
