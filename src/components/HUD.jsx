d523ce94ddbe88c95ea51f335b045e9ef7d3542c
Yusuf <moejoe1756@gmail.com>
2024-10-18 23:43:10 +0200

    Need to figure out emissiveness

--- src/components/HUD.jsx
+++ src/components/HUD.jsx
@@ -1,10 +1,11 @@
 import Speedometer from "./Speedometer";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export default function HUD() {
     return (
-        <Canvas style={{ background: 'gray', height: '100vh' }}>
-            <Speedometer speed={200}/>
+        <Canvas style={{ background: 'black', height: '100vh' }}>
+            <Speedometer speed={200}/>  
         </Canvas>
     );
 }
