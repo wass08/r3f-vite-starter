4342dfd0f7c5b25da4bf3ffd43b9576b34e57092
Mondo3771 <kabelomrn@gmail.com>
2024-10-20 13:37:27 +0200

    Added the Map

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,8 @@
 // import { Car } from "./components/Car";
 // import Model from "./assets/track/FullRaceTrackRaw";
 
-
 // // Main App Component
 // export default function App() {
-
 
 //   return (
 //     <Canvas shadows>
@@ -31,13 +29,11 @@
 
 //       </Physics>
 
-
 //       {/* OrbitControls */}
 //       <OrbitControls enablePan={false} enableZoom={true} enableRotate={true} />
 //     </Canvas>
 //   );
 // }
-
 
 /////////////////////////////////////
 
@@ -61,7 +57,7 @@
 //         {/* Uncomment these lines if you want to include the track models */}
 //         {/* <Model /> */}
 //         {/* <Race /> */}
-        
+
 //         {/* Ground plane */}
 //         <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
 //           <planeGeometry args={[400, 400]} />
@@ -74,7 +70,6 @@
 //     </Canvas>
 //   );
 // }
-
 
 // import React, { useRef } from "react";
 // import { Canvas } from "@react-three/fiber";
@@ -96,7 +91,7 @@
 //         {/* Uncomment these lines if you want to include the track models */}
 //         {/* <Model /> */}
 //         {/* <Race /> */}
-        
+
 //         {/* Ground plane */}
 //         <RigidBody type="fixed">
 //           <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
@@ -134,17 +129,14 @@
 //   );
 // }
 
-
-
-
 //////////////////////////////////
-
 
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { RigidBody, Physics } from "@react-three/rapier";
 import { Car } from "./components/Car";
-import Model from "./assets/track/FullRaceTrackRaw";
+import { RaceTrackWalls } from "./assets/track/Track1/CherryBlossomRawTrack";
+import { Map } from "./assets/track/Track1/WholeMap";
 
 // Main App Component
 export default function App() {
@@ -154,15 +146,14 @@ export default function App() {
       camera={{ fov: 60, near: 0.1, far: 2000, position: [0, 50, 200] }}
     >
       {/* Ambient and Directional Lighting */}
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={1} />
       <directionalLight
-        ref={directionalLightRef}
-        color={"#b77228"}
+        color={"#fbe8fd"}
         castShadow
-        position={[110, 150, 33]}
-        intensity={25}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
+        position={[85, 75, 0]}
+        intensity={10}
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
         shadow-camera-left={-500}
         shadow-camera-right={500}
         shadow-camera-top={500}
@@ -171,15 +162,26 @@ export default function App() {
         shadow-camera-far={1500}
         shadow-bias={-0.001}
       />
+      <Map />
 
-      <Physics gravity={[0, -90.81, 0]} debug>
+      <Physics gravity={[0, -50.81, 0]} debug >
         {/* Race track and ground */}
-        {/* <Model></Model>
-        <Race />  */}
-        <RigidBody type="fixed" position={[0, 0, 0]}>
-          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-            <planeGeometry args={[400, 400]} />
-            <meshStandardMaterial color="green" />
+
+        <RaceTrackWalls />
+
+        {/* Ground plane */}
+        <RigidBody type="fixed" position={[0, 0, 0]} >
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, 0, 0]}
+          >
+            <planeGeometry args={[1000, 1000]} />
+            <meshStandardMaterial
+              color="green"
+              transparent={true}
+              opacity={0}
+            />
           </mesh>
         </RigidBody>
 
