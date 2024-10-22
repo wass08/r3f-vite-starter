5b8b961e0e3b056db697999f91180b24ce0b4011
Nathan Joseph <nathanjoseph069@gmail.com>
2024-10-22 10:10:56 +0200

    HUD working

--- src/components/HUD.jsx
+++ src/components/HUD.jsx
@@ -6,20 +6,20 @@ import { Text } from "@react-three/drei";
 export default function HUD({currentLap, maxLap, speed, currentTime}) {
 
     return (
-        <Canvas style={{ background: 'gray', height: '100vh' }}>
+        <Canvas style={{ background: 'transparent', height: '100vh', zIndex:1 }}>
             <group position={[6.5,-3,0]}>
                 <Speedometer speed={speed}/>
             </group>
-            <Text position={[-6.5,3.3,0]} color="white" fontSize={0.9} >
+            {/* <Text position={[-6.5,3.3,0]} color="black" fontSize={0.9} >
                 {currentLap}/{maxLap}
-            </Text>  
-            <Text position={[-5,3.5,0]} color="white" fontSize={0.3} >
+            </Text>   */}
+            {/* <Text position={[-5,3.5,0]} color="black" fontSize={0.3} >
                 LAPS
-            </Text>
-            <Text position={[4,3.5,0]} color="white" fontSize={0.3} >
+            </Text> */}
+            {/* <Text position={[4,3.5,0]} color="black" fontSize={0.3} >
                 TIME
-            </Text>
-            <Text position={[6,3.3,0]} color="white" fontSize={0.9} >
+            </Text> */}
+            <Text position={[6,3.3,0]} color="black" fontSize={0.9} >
                 {currentTime}
             </Text> 
         </Canvas>
