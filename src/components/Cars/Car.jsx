2b3a0cfd9831c2c02b9786af2f184e9f15dd46ef
Mondo3771 <kabelomrn@gmail.com>
2024-11-02 17:22:29 +0200

    fixed car

--- src/components/Cars/Car.jsx
+++ src/components/Cars/Car.jsx
@@ -11,7 +11,7 @@ import { getBaseUrl } from "../../utils/getURL";
 import Checkpoint from "../Checkpoint";
 
 export function Car({ rigidBody, onSpeedChange,map, ...props }) {
-  const { nodes, materials } = useGLTF(`/Car.glb`);
+  const { nodes, materials } = useGLTF(`/~scarhatt/Car.glb`);
   // const rigidBody = useRef();
   const carRef=useRef();
   const cameraRef = useRef();
