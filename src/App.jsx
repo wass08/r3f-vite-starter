60d5516b0372a6331b2b651afe0ade0ebcb05e74
Daniel Daley <daniel.daley91@outlook.com>
2025-01-26 23:03:18 -0400

    Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,15 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+        <Avatar />
+      </group>
+
+      <ambientLight intensity={2} />
     </>
   );
 };
